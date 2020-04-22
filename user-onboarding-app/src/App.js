802320e56d9505a4bc1bf2820a68f4030f36390c
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-22 12:26:52 -0700

    added post request

--- user-onboarding-app/src/App.js
+++ user-onboarding-app/src/App.js
@@ -1,13 +1,79 @@
-import React from 'react';
-import Form from './Form'
+import React, {useState, useEffect} from 'react';
+import TeamMemberForm from './TeamMemberForm'
+import * as yup from 'yup'
+import axios from 'axios'
 import './App.css';
 
+
+const url = 'https://reqres.in/api/users'
+
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  name: '',
+  email: '',
+  ///// DROPDOWN /////
+  password: '',
+  ///// CHECKBOXES /////
+  hobbies: {
+    hiking: false,
+    reading: false,
+    coding: false,
+  },
+}
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+}
+
+const formSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(1, 'Name must have at least 1 characters')
+    .required('Name is required'),
+  email: yup
+    .string()
+    .email('Valid email is required')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
+      "Must Contain 6 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+    )
+    .required('Civil status is required')
+})
+
+
 function App() {
+  const [teamMembers, setTeamMembers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+const [formEnabled, setFormEnabled] = useState(false)
+const [formErrors, setFormErrors] = useState(initialFormErrors)
+
+const postTeamMember = teamMember => {
+  axios.post(url, teamMember)
+  .then(res => {
+    setTeamMembers(...teamMembers, res.data)
+  })
+  .catch(err => {
+    console.log('error')
+  })
+}
+
+
+
+
+
+
+
   return (
     <div className="App">
-    <Form>
-      
-    </Form>
+    <TeamMemberForm>
+
+    </TeamMemberForm>
     </div>
   );
 }
--- user-onboarding-app/src/TeamMemberForm.js
+++ user-onboarding-app/src/TeamMemberForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-const Form = () => {
+const TeamMemberForm = () => {
 
 
      return (
@@ -53,4 +53,4 @@ const Form = () => {
         )
 }
 
-export default Form
+export default TeamMemberForm
